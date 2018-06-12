e867a34d889998054287b521ebd7e0f8254b6692
githubwqj2 <2812930196@qq.com>
2018-06-12 21:59:33 +0800

    first commit

---
+++ src/main/java/TestJava.java
@@ -0,0 +1,7 @@
+/**
+ * @Auther: wqj
+ * @Date: 2018/6/12 21:54
+ * @Description:
+ */
+public class TestJava {
+}
---
+++ src/main/scala/TestScala.scala
@@ -0,0 +1,8 @@
+/**
+  * @Auther: wqj
+  * @Date: 2018/6/12 21:53
+  * @Description:
+  */
+object TestScala {
+
+}
