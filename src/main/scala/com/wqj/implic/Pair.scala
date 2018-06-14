da405755face9fd84c8dfd6f8ac60b39bf3762fc
githubwqj <893453172@qq.com>
2018-06-14 15:58:36 +0800

    台式机提交

--- src/main/scala/com/wqj/implic/Pair.scala
+++ src/main/scala/com/wqj/implic/Pair.scala
@@ -3,12 +3,24 @@ package com.wqj.implic
 /**
   * @Auther: wqj
   * @Date: 2018/6/14 15:42
-  * @Description:  学习限制类型
+  * @Description: 学习限制类型
   */
 class Pair[T <: Comparable[T]] {
 
   // T 必须实现Comparable接口,是Comparable的子类
-  def bigger(first:T,second: T)={
-    if (first > second) first else second
+  def bigger(first: T, second: T) = {
+    if (first.compareTo(second) > 0)
+      first
+    else
+      second
+  }
+
+}
+
+object Pair {
+
+  def main(args: Array[String]): Unit = {
+    val p = new Pair[String]()
+    print(p.bigger("a", "b"))
   }
 }
